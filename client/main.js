caca7d0ee51c5d77d9df6c5429d373ac93dc7283
danjelous <daniel.brunner@live.at>
2018-02-21 15:29:07 +0100

    Add scss support and scale html to max size (catch all clicks)

--- client/main.js
+++ client/main.js
@@ -3,8 +3,10 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+const userID = 'A'
+
 Template.root.events({
   'click': (event) => {
-    console.log(`Registered click event`);
+    console.log(`Registered click event from user ${userID}.`);
   }
 });
